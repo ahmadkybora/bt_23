b766d088d749c64b834c310892b20d8adf4a2d26
montazeri_a <montazeri_a@office.zharfpouyan.net>
2022-11-05 10:17:39 +0330

    ..

--- Cover.py
+++ Cover.py
@@ -1,4 +1,5 @@
 import logging
+import requests
 import os
 
 import music_tag
@@ -440,7 +441,34 @@ def handle_download_message(update: Update, context: CallbackContext) -> None:
         changing_url = instagram_post.split("/")
         url_code = changing_url[4]
         url = f"https://instagram.com/p/{url_code}?__a=1"
-        logging.error(url_code)
+
+        try:
+            global checking_video
+            visit = requests.get(url).json()
+            checking_video = visit['graphql']['shortcode_media']['is_video']
+        except:
+            context.bot.sendMessage(chat_id=update.message.chat_id, text="Send Me Only Public Instagram Posts ⚡️")
+        
+        if checking_video==True:
+            try:
+                video_url = visit['graphql']['shortcode_media']['video_url']
+                context.bot.send_chat_action(chat_id=update.message.chat_id, action="upload_video")
+                context.bot.sendVideo(chat_id=update.message.chat_id, video=video_url)
+            except:
+                pass
+
+        elif checking_video==False:
+            try:
+                post_url = visit['graphql']['shortcode_media']['display_url']
+                context.bot.send_chat_action(chat_id=update.message.chat_id, action="upload_photo")
+                context.bot.sendPhoto(chat_id=update.message.chat_id, photo=post_url)
+            except:
+                pass
+        else:
+            context.bot.send_chat_action(chat_id=update.message.chat_id, action="typing")
+            context.bot.sendMessage(chat_id=update.message.chat_id, text="I Cant Send You Private Posts :-( ")
+    else:
+        context.bot.sendMessage(chat_id=update.message.chat_id, text="Kindly Send Me Public Instagram Video/Photo Url")
 
     # user_id = update.effective_user.id
     # # context.bot.send_document(user_id, message.text)
